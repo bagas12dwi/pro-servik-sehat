a8a89af060c9b636ca96c77b73995adba54032d8
bagas12dwi <bag12dwi.s@gmail.com>
2024-12-30 04:26:17 +0700

    add nullable

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $bookingDate = Booking::where('user_id', auth()->user()->id)
+        $bookingDate = Booking::where('user_id', auth()->user()->id ?? '')
             ->orderBy('inspection_date', 'desc') // Mengurutkan berdasarkan tanggal terbaru
             ->pluck('inspection_date')
             ->first();
